621c0964a287666875845e0a3965ff07d3339dc9
Chris Stryczynski <chris@soundshare.co.uk>
2018-12-14 08:30:00 +0000

    export per week etc

--- src/Export.hs
+++ src/Export.hs
@@ -9,25 +9,79 @@ import Data.Time.Calendar
 import Data.Time.Calendar.OrdinalDate
 import Control.Lens.Operators
 import Data.Aeson
+import Options.Applicative
+import Control.Monad (join)
+import Data.Monoid ((<>))
+import Text.Pretty.Simple (pPrint)
 
 import Common
 import Log
 
+exportEntries :: [Entry] -> IO ()
+exportEntries x = do
+  encodeFile "log_export.json"
+    $  ("Task","desc","start","end", "duration")
+        : (fmap
+          (\(Entry t d s e) ->
+              ( t
+              , d
+              , myFormatUtcTimeSpeardsheetFriendly s
+              , myFormatUtcTimeSpeardsheetFriendly e
+              , myFormatDiffTime $ diffUTCTime e s
+            )
+          )
+          x
+      )
+
 export :: IO ()
 export = do
   validEntries >>= \case
-    Right x -> do
-      encodeFile "log_export.json"
-        $  ("Task","desc","start","end", "duration")
-           : (fmap
-              (\(Entry t d s e) ->
-                 ( t
-                 , d
-                 , myFormatUtcTimeSpeardsheetFriendly s
-                 , myFormatUtcTimeSpeardsheetFriendly e
-                 , myFormatDiffTime $ diffUTCTime e s
-                )
-              )
-              x
-          )
+    Right x -> exportEntries x
     Left e -> error e
+
+exportWeek  :: IO ()
+exportWeek = do
+  validEntries >>= \case
+    Right x -> exportEntries $ filter (const True) x
+    Left e -> error e
+
+main :: IO ()
+main = join . customExecParser (prefs showHelpOnError) $
+  info (helper <*> parser)
+  (  fullDesc
+  <> header "list directories"
+  )
+  where
+    parser :: Parser (IO ())
+    parser =
+      work
+        <$> switch
+            (  long "this-week"
+            <> help ""
+            <> showDefault
+            )
+        <*> 
+            switch
+            (  long "this-week-per-day"
+            <> help ""
+            <> showDefault
+            )
+
+work :: Bool -> Bool -> IO ()
+work True _  = do
+  exportWeek
+  print "Exported this week successful"
+work _ True = do
+  ct <- getCurrentTime
+  validEntries >>= \case
+    Right x -> do
+      let x' = getDurationPerDayInWeek x ct
+      let x'' = getDurationPerDayInWeek x (addUTCTime ((-nominalDay)) ct)
+      pPrint $ fmap (\(a,b) -> (a, myFormatDiffTime b)) x''
+      print "Exported previous"
+      pPrint $ fmap (\(a,b) -> (a, myFormatDiffTime b)) x'
+      print "Exported this week per day"
+    Left e -> error e
+work False False = do
+  export
+  print "Exported all successful"
